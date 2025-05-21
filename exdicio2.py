d8a780002aec5275a180fce90143108c318872ee
Eduarda Coelho <168098260+adudacoelho@users.noreply.github.com>
2025-05-21 10:15:30 -0300

    Update exdicio2.py

--- exdicio2.py
+++ exdicio2.py
@@ -1,15 +1,15 @@
 def main():
 
    
-dIN = {'César Hilal': 175, 'Aldo Carvalho': 180, 'Maria Eleonora': 165, 'Pedro Cunha': 190}
-dOUT = {}
+  dIN = {'César Hilal': 175, 'Aldo Carvalho': 180, 'Maria Eleonora': 165, 'Pedro Cunha': 190}
+  dOUT = {}
 
-print ("ENTRADA", din)
+  print ("ENTRADA", dIN)
 
-for k in dIN.keys():
-  if dIN[k] > 170:
-dOUT[k] = dIN[k]
+  for k in dIN.keys():
+    if dIN[k] > 170:
+      dOUT[k] = dIN[k]
 
-print("Saída, dOUT)
+  print("SAÍDA", dOUT)
 
 main()
