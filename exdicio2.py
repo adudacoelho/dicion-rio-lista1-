678df01c4e28e6e9768181476df2111924ed71a3
Eduarda Coelho <168098260+adudacoelho@users.noreply.github.com>
2025-05-21 08:59:37 -0300

    Add files via upload

---
+++ exdicio2.py
@@ -0,0 +1,15 @@
+def main():
+
+   
+dIN = {'César Hilal': 175, 'Aldo Carvalho': 180, 'Maria Eleonora': 165, 'Pedro Cunha': 190}
+dOUT = {}
+
+print ("ENTRADA", din)
+
+for k in dIN.keys():
+  if dIN[k] > 170:
+dOUT[k] = dIN[k]
+
+print("Saída, dOUT)
+
+main()
